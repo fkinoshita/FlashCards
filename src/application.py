b4df484a1ce9a99d6d1d028b2aecfe4d52d7eace
Felipe Kinoshita <kinofhek@gmail.com>
2023-05-27 00:35:28 -0300

    main: Move Application class to separate file

---
+++ src/application.py
@@ -0,0 +1,58 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import sys
+import gi
+
+from gi.repository import Adw, Gtk, Gio
+
+from .window import Window
+
+
+class Application(Adw.Application):
+    """The main application singleton class."""
+
+    def __init__(self):
+        super().__init__(application_id='io.github.fkinoshita.FlashCards',
+                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+        self.create_action('quit', lambda *_: self.quit(), ['<primary>q'])
+        self.create_action('close', lambda *_: self.props.active_window.close(), ['<primary>w'])
+        self.create_action('about', self.on_about_action)
+
+
+    def do_activate(self):
+        """Called when the application is activated.
+
+        We raise the application's main window, creating it if
+        necessary.
+        """
+        win = self.props.active_window
+        if not win:
+            win = Window(application=self)
+        win.present()
+
+
+    def on_about_action(self, widget, _):
+        """Callback for the app.about action."""
+        builder = Gtk.Builder.new_from_resource(
+            "/io/github/fkinoshita/FlashCards/about_dialog.ui"
+        )
+        about_dialog = builder.get_object("about_dialog")
+        about_dialog.set_transient_for(self.props.active_window)
+        about_dialog.present()
+
+
+    def create_action(self, name, callback, shortcuts=None):
+        """Add an application action.
+
+        Args:
+            name: the name of the action
+            callback: the function to be called when the action is
+              activated
+            shortcuts: an optional list of accelerators
+        """
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
--- src/main.py
+++ src/main.py
@@ -7,66 +7,11 @@ gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
 from gi.repository import Adw, Gtk, Gio
-from .window import Window
-
-
-class FlashcardsApplication(Adw.Application):
-    """The main application singleton class."""
-
-    def __init__(self):
-        super().__init__(application_id='io.github.fkinoshita.FlashCards',
-                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
-        self.create_action('quit', lambda *_: self.quit(), ['<primary>q'])
-        self.create_action('close', lambda *_: self.props.active_window.close(), ['<primary>w'])
-        self.create_action('about', self.on_about_action)
-        self.create_action('preferences', self.on_preferences_action)
-
-
-    def do_activate(self):
-        """Called when the application is activated.
-
-        We raise the application's main window, creating it if
-        necessary.
-        """
-        win = self.props.active_window
-        if not win:
-            win = Window(application=self)
-        win.present()
-
-
-    def on_about_action(self, widget, _):
-        """Callback for the app.about action."""
-        builder = Gtk.Builder.new_from_resource(
-            "/io/github/fkinoshita/FlashCards/about_dialog.ui"
-        )
-        about_dialog = builder.get_object("about_dialog")
-        about_dialog.set_transient_for(self.props.active_window)
-        about_dialog.present()
-
-
-
-    def on_preferences_action(self, widget, _):
-        """Callback for the app.preferences action."""
-        print('app.preferences action activated')
-
-
-    def create_action(self, name, callback, shortcuts=None):
-        """Add an application action.
-
-        Args:
-            name: the name of the action
-            callback: the function to be called when the action is
-              activated
-            shortcuts: an optional list of accelerators
-        """
-        action = Gio.SimpleAction.new(name, None)
-        action.connect("activate", callback)
-        self.add_action(action)
-        if shortcuts:
-            self.set_accels_for_action(f"app.{name}", shortcuts)
+from .application import Application
 
 
 def main(version):
     """The application's entry point."""
-    app = FlashcardsApplication()
+    app = Application()
     return app.run(sys.argv)
+
